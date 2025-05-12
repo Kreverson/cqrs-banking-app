5ca6105957787139e2c2643ea63ef0627bece40c
Kreverson <kreversonjulho@gmail.com>
2025-05-12 14:31:23 -0300

    Created folders structure for liquidBase and sql files to generates tables structure. Added docker compose file and adjusts some classes

--- src/main/java/br/com/cqrs/banking/app/domain/model/Card.java
+++ src/main/java/br/com/cqrs/banking/app/domain/model/Card.java
@@ -2,8 +2,8 @@ package br.com.cqrs.banking.app.domain.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.Transaction;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Card {
 
     @Id
--- src/main/java/br/com/cqrs/banking/app/repository/EventRepository.java
+++ src/main/java/br/com/cqrs/banking/app/repository/EventRepository.java
@@ -1,7 +1,7 @@
 package br.com.cqrs.banking.app.repository;
 
-import br.com.cqrs.banking.app.events.Event;
+import br.com.cqrs.banking.app.events.AbstractEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<AbstractEvent, Long> {
 }
