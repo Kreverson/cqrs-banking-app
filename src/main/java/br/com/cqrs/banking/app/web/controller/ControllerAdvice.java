3e6fe660bb72eb3ad4e6af09a77871e416b08fdd
Kreverson <kreversonjulho@gmail.com>
2025-05-16 04:41:59 -0300

    Added more default messages errors for Controller advice

--- src/main/java/br/com/cqrs/banking/app/web/controller/ControllerAdvice.java
+++ src/main/java/br/com/cqrs/banking/app/web/controller/ControllerAdvice.java
@@ -1,11 +1,18 @@
 package br.com.cqrs.banking.app.web.controller;
 
+import br.com.cqrs.banking.app.domain.exception.ResourceAlreadyExistsException;
 import br.com.cqrs.banking.app.domain.exception.ResourceNotFoundException;
 import br.com.cqrs.banking.app.web.dto.MessageDto;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -16,5 +23,49 @@ public class ControllerAdvice {
             final ResourceNotFoundException e
             ) {
         return new MessageDto(e.getMessage() != null ? e.getMessage() : "Not found");
+    }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageDto resourceAlreadyExists(
+            final ResourceAlreadyExistsException e
+    ) {
+        return new MessageDto(e.getMessage() != null ? e.getMessage() : "Already exists");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageDto validation(
+            final MethodArgumentNotValidException e
+    ) {
+        Map<String, String> errors = e.getBindingResult()
+                .getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        DefaultMessageSourceResolvable::getDefaultMessage,
+                        (existingResult, newMessage) -> existingResult + " " + newMessage
+                ));
+
+        return new MessageDto(
+                "Validation failed",
+                errors
+        );
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public MessageDto illegalState(
+            final IllegalStateException e
+    ) {
+        return new MessageDto(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public MessageDto exception(
+            final Exception e
+    ) {
+        e.printStackTrace();
+        return new MessageDto("Server error");
     }
 }
